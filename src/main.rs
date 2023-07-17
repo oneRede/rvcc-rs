f39c060231ad9e4134a7b104a4157ccb3795a6b1
rede <rede16724@gmail.com>
2023-07-17 11:52:55 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::{env, num::ParseIntError, process::exit, slice};
+use std::{env, num::ParseIntError, process::exit, slice, str::Chars};
 mod utils;
 
-use utils::get_str_num;
+use utils::{get_num_from_chars, get_str_num};
 
 #[allow(dead_code)]
-#[derive(PartialEq)]
+#[derive(Clone, Copy, PartialEq)]
 enum TokenKind {
     Punct,
     Num,
@@ -12,23 +12,23 @@ enum TokenKind {
 }
 
 #[allow(dead_code)]
-struct Token<'a> {
+struct Token {
     kind: TokenKind,
-    next: Option<*mut Token<'a>>,
+    next: Option<*mut Token>,
     val: i32,
-    loc: Option<*const str>,
+    loc: Option<&'static [char]>,
     len: usize,
 }
 
 #[allow(dead_code)]
-impl<'a> Token<'a> {
-    fn new(token_kind: TokenKind, loc: *const u8, end: usize) -> Self {
+impl Token {
+    fn new(token_kind: TokenKind, loc: &'static [char], len: usize) -> Self {
         Self {
             kind: token_kind,
             next: None,
             val: 0,
-            loc: None,
-            len: 0,
+            loc: Some(loc),
+            len: len,
         }
     }
     fn empty() -> Self {
@@ -43,15 +43,8 @@ impl<'a> Token<'a> {
 }
 
 #[allow(dead_code)]
-fn equal(token: &Token, s: &str) -> bool {
-    if unsafe {
-        std::str::from_utf8(slice::from_raw_parts(
-            token.loc.unwrap() as *const u8,
-            token.len,
-        ))
-        .unwrap()
-    } == s
-    {
+fn equal(token: &Token, s: &[char]) -> bool {
+    if s.starts_with(unsafe { slice::from_raw_parts(token.loc.unwrap().as_ptr(), token.len) }) {
         return true;
     } else {
         return false;
@@ -59,15 +52,15 @@ fn equal(token: &Token, s: &str) -> bool {
 }
 
 #[allow(dead_code)]
-fn skip<'a>(token: Token<'a>, s: &'a str) -> Option<*mut Token<'a>> {
+fn skip<'a>(token: &Token, s: &[char]) -> Option<*mut Token> {
     if !equal(&token, s) {
-        println!("expect '{}'", s)
+        println!("expect '{:?}'", s)
     }
     return token.next;
 }
 
 #[allow(dead_code)]
-fn get_num(token: Token) -> i32 {
+fn get_num(token: &Token) -> i32 {
     if token.kind != TokenKind::Num {
         println!("expect a num")
     }
@@ -75,28 +68,51 @@ fn get_num(token: Token) -> i32 {
 }
 
 #[allow(dead_code)]
-fn tokenize(mut s: &'static str) {
+fn tokenize(mut chars: &'static [char]) -> Option<*mut Token> {
     let head: *mut Token = Box::leak(Box::new(Token::empty()));
     let mut cur = head;
 
     loop {
-        if s.len() == 0 {
-            break;
+        if chars.len() == 0 {
+            unsafe {
+                cur.as_mut().unwrap().next = Some(Box::leak(Box::new(Token::new(TokenKind::Eof, chars, 0))))
+            };
+            return unsafe { head.as_mut().unwrap().next }
         }
 
-        let c: Vec<char> = s[..1].chars().collect();
-        if c.get(0).unwrap().is_whitespace() {
-            s = &s[1..];
+        let c: char = chars[0];
+        if c.is_whitespace() {
+            chars = &chars[1..];
             continue;
         }
 
-        let num_rs: Result<i32, ParseIntError> = s.parse();
-        match num_rs {
-            Ok(num) => {}
-            Err(e) => {
-                continue;
+        let num_rs = get_num_from_chars(chars);
+        if let Ok((num, cs)) = num_rs {
+            chars = cs;
+
+            unsafe {
+                cur.as_mut().unwrap().next = Some(Box::leak(Box::new(Token::new(
+                    TokenKind::Num,
+                    chars,
+                    num.to_string().len(),
+                ))));
             }
+            cur = unsafe { cur.as_mut().unwrap().next.unwrap() };
+            unsafe { cur.as_mut().unwrap().val = num };
+            unsafe { cur.as_mut().unwrap().len = num.to_string().len() };
+            continue;
         }
+
+        if chars[0] == '+' || chars[0] == '-' {
+            unsafe {
+                cur.as_mut().unwrap().next =
+                    Some(Box::leak(Box::new(Token::new(TokenKind::Punct, chars, 1))))
+            };
+            cur = unsafe { cur.as_mut().unwrap().next.unwrap() };
+            chars = &chars[1..];
+            continue;
+        }
+        println!("invalid token: {}", chars[0]);
     }
 }
 
@@ -106,143 +122,39 @@ fn main() {
         println!("{}: invalid number of arguments\n", &args.get(0).unwrap());
         exit(1)
     }
-    let mut p: &str = &args[1];
+    let chars: Vec<char> = args[1].chars().collect();
+    let chars: &[char] = Box::leak(Box::new(chars));
+    let mut token = tokenize(chars);
 
     println!("  .globl main");
     println!("main:");
-    let (n, s) = get_str_num(p);
-    p = s;
-    println!("  li a0, {}", n);
-
+    println!(
+        "  li a0, {}",
+        get_num(unsafe { token.unwrap().as_ref().unwrap() })
+    );
+    token = unsafe { token.unwrap().as_ref().unwrap().next };
     loop {
-        if p.len() == 0 {
+        if unsafe { token.unwrap().as_ref().unwrap().kind } == TokenKind::Eof {
             break;
         }
-        if p.starts_with("+") {
-            let (n, s) = get_str_num(&p[1..]);
-            println!("  addi a0, a0, {}", n);
-            p = s;
-        } else if p.starts_with("-") {
-            let (n, s) = get_str_num(&p[1..]);
-            println!("  addi a0, a0, -{}", n);
-            p = s;
-        } else {
-            println!("unexcept character: {}", &p[..1]);
-            exit(1)
+        if equal(unsafe { token.unwrap().as_ref().unwrap() }, &['+']) {
+            token = unsafe { token.unwrap().as_ref().unwrap().next };
+            println!(
+                "  addi a0, a0, {}",
+                get_num(unsafe { token.unwrap().as_ref().unwrap() })
+            );
+            token = unsafe { token.unwrap().as_ref().unwrap().next };
+            continue;
         }
+
+        token = skip(unsafe { token.unwrap().as_ref().unwrap() }, &['-']);
+        println!(
+            "  addi a0, a0, -{}",
+            get_num(unsafe { token.unwrap().as_ref().unwrap() })
+        );
+        token = unsafe { token.unwrap().as_ref().unwrap().next };
     }
 
     println!("  ret");
     exit(0)
 }
-
-// typedef enum {
-//     TK_PUNCT, // 操作符如： + -
-//     TK_NUM,   // 数字
-//     TK_EOF,   // 文件终止符，即文件的最后
-//   } TokenKind;
-
-//   // 终结符结构体
-//   typedef struct Token Token;
-//   struct Token {
-//     TokenKind Kind; // 种类
-//     Token *Next;    // 指向下一终结符
-//     int Val;        // 值
-//     char *Loc;      // 在解析的字符串内的位置
-//     int Len;        // 长度
-//   };
-
-//   // 输出错误信息
-//   // static文件内可以访问的函数
-//   // Fmt为传入的字符串， ... 为可变参数，表示Fmt后面所有的参数
-//   static void error(char *Fmt, ...) {
-//     // 定义一个va_list变量
-//     va_list VA;
-//     // VA获取Fmt后面的所有参数
-//     va_start(VA, Fmt);
-//     // vfprintf可以输出va_list类型的参数
-//     vfprintf(stderr, Fmt, VA);
-//     // 在结尾加上一个换行符
-//     fprintf(stderr, "\n");
-//     // 清除VA
-//     va_end(VA);
-//     // 终止程序
-//     exit(1);
-//   }
-
-//   // 判断Tok的值是否等于指定值，没有用char，是为了后续拓展
-//   static bool equal(Token *Tok, char *Str) {
-//     // 比较字符串LHS（左部），RHS（右部）的前N位，S2的长度应大于等于N.
-//     // 比较按照字典序，LHS<RHS回负值，LHS=RHS返回0，LHS>RHS返回正值
-//     // 同时确保，此处的Op位数=N
-//     return memcmp(Tok->Loc, Str, Tok->Len) == 0 && Str[Tok->Len] == '\0';
-//   }
-
-//   // 跳过指定的Str
-//   static Token *skip(Token *Tok, char *Str) {
-//     if (!equal(Tok, Str))
-//       error("expect '%s'", Str);
-//     return Tok->Next;
-//   }
-
-//   // 返回TK_NUM的值
-//   static int getNumber(Token *Tok) {
-//     if (Tok->Kind != TK_NUM)
-//       error("expect a number");
-//     return Tok->Val;
-//   }
-
-//   // 生成新的Token
-//   static Token *newToken(TokenKind Kind, char *Start, char *End) {
-//     // 分配1个Token的内存空间
-//     Token *Tok = calloc(1, sizeof(Token));
-//     Tok->Kind = Kind;
-//     Tok->Loc = Start;
-//     Tok->Len = End - Start;
-//     return Tok;
-//   }
-
-//   // 终结符解析
-//   static Token *tokenize(char *P) {
-//     Token Head = {};
-//     Token *Cur = &Head;
-
-//     while (*P) {
-//       // 跳过所有空白符如：空格、回车
-//       if (isspace(*P)) {
-//         ++P;
-//         continue;
-//       }
-
-//       // 解析数字
-//       if (isdigit(*P)) {
-//         // 初始化，类似于C++的构造函数
-//         // 我们不使用Head来存储信息，仅用来表示链表入口，这样每次都是存储在Cur->Next
-//         // 否则下述操作将使第一个Token的地址不在Head中。
-//         Cur->Next = newToken(TK_NUM, P, P);
-//         // 指针前进
-//         Cur = Cur->Next;
-//         const char *OldPtr = P;
-//         Cur->Val = strtoul(P, &P, 10);
-//         Cur->Len = P - OldPtr;
-//         continue;
-//       }
-
-//       // 解析操作符
-//       if (*P == '+' || *P == '-') {
-//         // 操作符长度都为1
-//         Cur->Next = newToken(TK_PUNCT, P, P + 1);
-//         Cur = Cur->Next;
-//         ++P;
-//         continue;
-//       }
-
-//       // 处理无法识别的字符
-//       error("invalid token: %c", *P);
-//     }
-
-//     // 解析结束，增加一个EOF，表示终止符。
-//     Cur->Next = newToken(TK_EOF, P, P);
-//     // Head无内容，所以直接返回Next
-//     return Head.Next;
-//   }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::{env::consts, mem};
+use std::{env::consts, mem, process::exit, num::ParseIntError, f32::consts::E};
 
 #[allow(dead_code)]
 pub fn get_str_num(s: &str) -> (isize, &str) {
@@ -16,6 +16,32 @@ pub fn get_str_num(s: &str) -> (isize, &str) {
     }
 
     return ((&s[..i]).parse().unwrap(), &s[i..]);
+}
+
+#[allow(dead_code)]
+pub fn get_num_from_chars(s: &[char]) -> Result<(i32, &[char]), ParseIntError> {
+    let mut i: usize = 0;
+    let mut num_string = "".to_string();
+    for c in s {
+        match c {
+            '0'..='9' => {
+                i += 1;
+                num_string += c.to_string().as_ref();
+            }
+            '-' | '+' => {
+                break;
+            }
+            _ => {
+                println!("unexcept character: {}", c);
+                exit(1)
+            }
+        }
+    }
+    let rs: Result<i32, ParseIntError> = num_string.parse();
+    match rs {
+        Ok(num) => {return Ok((num, &s[i..]))},
+        Err(e) => {return Err(e)}
+    }
 }
 
 #[allow(dead_code)]
@@ -50,13 +76,13 @@ fn test_is_digit() {
 }
 
 #[test]
-fn test_eof(){
+fn test_eof() {
     let s = "1234567890";
     println!("{:?}", s.as_bytes())
 }
 
 #[test]
-fn test_str_p(){
+fn test_str_p() {
     let s = "1234567890";
     let mut p = s.as_ptr();
     p = unsafe { p.add(2) };
@@ -65,21 +91,22 @@ fn test_str_p(){
 }
 
 #[test]
-fn test_p_u8(){
+fn test_p_u8() {
     let s = "";
     println!("{:?}", &s.as_ptr())
 }
 
 #[test]
-fn test_p_str(){
+fn test_p_str() {
     let s = "1234567890".to_string();
     let p: *const String = Box::leak(Box::new(s));
     println!("{:?}", p);
     println!("{}", mem::size_of::<*const String>());
-    println!("{:?}", unsafe{p.add(2)});
-    println!("{:?}", unsafe{(p as *mut u8).add(1)});
+    println!("{:?}", unsafe { p.add(2) });
+    println!("{:?}", unsafe { (p as *mut u8).add(1) });
     println!("{}", mem::size_of::<*const u8>());
     println!("{}", mem::size_of::<Box<u8>>());
     println!("{}", mem::size_of::<Vec<String>>());
-    println!("{}", mem::size_of::<Vec<u8>>())
+    println!("{}", mem::size_of::<Vec<u8>>());
+    println!("char: {}", mem::size_of::<char>());
 }
