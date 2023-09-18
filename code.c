de2a7a62afc593affd6c5cf40ecbbed3a757a682
rede16724@gmail.com <rede16724@gmail.com>
2023-09-18 19:50:44 +0800

    finish course 59

--- code.c
+++ code.c
@@ -1,4 +1,4 @@
 int main()
 {
-   assert(16, ({ struct t {int a; int b;} x; struct t y; sizeof(y); }), "({ struct t {int a; int b;} x; struct t y; sizeof(y); })");
+   assert(8, ({ char (*x)[3]; sizeof(x); }));
 }
